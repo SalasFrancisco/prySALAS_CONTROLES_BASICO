4b848597234dfa1bbb33c4d708a75df755f0c402
Alumnos <Alumnos@PC190>
2023-05-12 13:25:55 -0300

    Agregar archivos de proyecto.

---
+++ prySALAS_CONTROLES_BASICO/frmPrincipal.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace prySALAS_CONTROLES_BASICO
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEjeX = new System.Windows.Forms.Label();
+            this.lblEjeY = new System.Windows.Forms.Label();
+            this.lblBienvenida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEjeX
+            // 
+            this.lblEjeX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeX.AutoSize = true;
+            this.lblEjeX.BackColor = System.Drawing.Color.Black;
+            this.lblEjeX.Font = new System.Drawing.Font("Lucida Sans Unicode", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjeX.ForeColor = System.Drawing.Color.Lime;
+            this.lblEjeX.Location = new System.Drawing.Point(774, 9);
+            this.lblEjeX.Name = "lblEjeX";
+            this.lblEjeX.Size = new System.Drawing.Size(15, 15);
+            this.lblEjeX.TabIndex = 0;
+            this.lblEjeX.Text = "X";
+            // 
+            // lblEjeY
+            // 
+            this.lblEjeY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeY.AutoSize = true;
+            this.lblEjeY.BackColor = System.Drawing.Color.Black;
+            this.lblEjeY.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjeY.ForeColor = System.Drawing.Color.Lime;
+            this.lblEjeY.Location = new System.Drawing.Point(774, 31);
+            this.lblEjeY.Name = "lblEjeY";
+            this.lblEjeY.Size = new System.Drawing.Size(15, 13);
+            this.lblEjeY.TabIndex = 1;
+            this.lblEjeY.Text = "Y";
+            // 
+            // lblBienvenida
+            // 
+            this.lblBienvenida.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBienvenida.AutoSize = true;
+            this.lblBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenida.Location = new System.Drawing.Point(264, 193);
+            this.lblBienvenida.Name = "lblBienvenida";
+            this.lblBienvenida.Size = new System.Drawing.Size(250, 25);
+            this.lblBienvenida.TabIndex = 2;
+            this.lblBienvenida.Text = "Bienvenido al Formulario";
+            this.lblBienvenida.Visible = false;
+            this.lblBienvenida.MouseEnter += new System.EventHandler(this.lblBienvenida_MouseEnter);
+            this.lblBienvenida.MouseHover += new System.EventHandler(this.lblBienvenida_MouseHover);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBienvenida);
+            this.Controls.Add(this.lblEjeY);
+            this.Controls.Add(this.lblEjeX);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPrincipal";
+            this.Text = "Titulo de Ventana";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Click += new System.EventHandler(this.frmPrincipal_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmPrincipal_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEjeX;
+        private System.Windows.Forms.Label lblEjeY;
+        private System.Windows.Forms.Label lblBienvenida;
+    }
+}
---
+++ prySALAS_CONTROLES_BASICO/frmPrincipal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySALAS_CONTROLES_BASICO
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEjeX.Text = e.X.ToString();
+            lblEjeY.Text = e.Y.ToString();
+        }
+
+        private void frmPrincipal_Click(object sender, EventArgs e)
+        {
+            lblBienvenida.Visible = true;
+        }
+
+        private void lblBienvenida_MouseHover(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblBienvenida_MouseEnter(object sender, EventArgs e)
+        {
+            lblBienvenida.Location = new Point();
+        }
+
+    }
+}
